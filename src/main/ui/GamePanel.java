e439cead65f6a389f0a9b60598655b65e50a8d2f
Eli Gu <fgu05@students.cs.ubc.ca>
2023-11-04 01:26:10 -0700

    update operation gui

--- src/main/ui/GamePanel.java
+++ src/main/ui/GamePanel.java
@@ -1,18 +1,9 @@
 package ui;
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialogBuilder;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import model.*;
+import model.FBGame;
+import model.Leaderboard;
+import model.Score;
+import model.Tube;
 import persistence.FBGameJsonReader;
 import persistence.FBGameJsonWriter;
 
@@ -244,6 +235,7 @@ public class GamePanel extends JPanel implements MouseListener {
         }
     }
 
+    //process the keystroke input by user
     public void processKey(int keyCode) {
         if (keyCode == KeyEvent.VK_SPACE) {
             game.getBird().flap();
